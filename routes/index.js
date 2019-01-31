80f45663d617714759b7d4eb5d6c2201d0d41480
unknown <kd00441@gmail.com>
2019-01-30 19:22:13 -0500

    added expressJS support

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,19 @@ let router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'home' });
+});
+/* GET abt page. */
+router.get('/about', function(req, res, next) {
+  res.render('index', { title: 'about' });
+});
+/* GET contact page. */
+router.get('/contact', function(req, res, next) {
+  res.render('index', { title: 'contact' });
+});
+/* GET products page. */
+router.get('/product', function(req, res, next) {
+  res.render('index', { title: 'products' });
 });
 
 module.exports = router;
