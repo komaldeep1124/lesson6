89c93025f9e147c4063b582d09ede79b83abe37a
unknown <kd00441@gmail.com>
2019-02-10 00:37:20 -0500

    display db in view

--- routes/contact.js
+++ routes/contact.js
@@ -1,22 +1,26 @@
 let express = require('express');
 let router = express.Router();
-let mongoose=require('mongoose');
-//create a refernce to db schema
-let contact=require('../models/conatct');
-// get contact list page=read operation
-router.get('/',(requesr,res,next)=>{
-    contact.find((err,contactList)=>{
-if(err){
-return console.error(err);
-}
-else{
-    console.log(contactList);
-}
-/*res.rener('contact/index'),{
-    title:'Contact List',
-    contactList=contactList;
-}):*/
+let mongoose = require('mongoose');
+
+// create a reference to the db schema
+let contact = require('../models/contact');
+
+/* GET Contact List page - READ Operation */
+router.get('/', (req, res, next) =>{
+    contact.find((err, contactList) => {
+        if(err) {
+            return console.error(err);
+        }
+        else {
+           // console.log(contactList);
+
+            res.render('contacts/index', {
+                title: 'Contact List',
+                contactList: contactList
+            });
+            
+        }
     });
 });
-router
+
 module.exports = router;
