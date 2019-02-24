5a8444d9ea5a62a2fe8c63a14f94770abecb2342
unknown <kd00441@gmail.com>
2019-02-24 00:45:28 -0500

    reorganized project structure

--- server/config/app.js
+++ server/config/app.js
@@ -3,9 +3,9 @@ let express = require('express');
 let path = require('path');
 let cookieParser = require('cookie-parser');
 let logger = require('morgan');
-//database Setup 
+//database setup 
 let mongoose=require('mongoose');
-let DB= require('./config/db');
+let DB= require('./db');
 // point mongoose to URI
 mongoose.connect(DB.URI);
  let mongoDB=mongoose.connection;
@@ -13,21 +13,21 @@ mongoose.connect(DB.URI);
  mongoDB.once('open',()=>{
    console.log("Connected to MongoDB...");
  });
-let indexRouter = require('./routes/index');
-let contactRouter=require('./routes/contact');
+let indexRouter = require('../routes/index');
+let contactRouter=require('../routes/contact');
 
 let app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, '../views'));
 app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.static(path.join(__dirname, 'node_modules')));
+app.use(express.static(path.join(__dirname, '../../public')));
+app.use(express.static(path.join(__dirname, '../../node_modules')));
 
 app.use('/', indexRouter);
 app.use('/contact-list',contactRouter);
